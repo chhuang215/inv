05267eec598cb03491c1376c0fbad8955b00422e
Chih-Hsuan Huang <chhuang215@gmail.com>
2019-04-01 20:49:37 +0800

    more scripts, animation. added toggle attendence

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 var submitted=false;
 
-function formSubmit(){
+var formSubmit = function(){
     if(submitted)  {
         $('#msgTY').show()
     }
 }
+
+$('#chkCy').change(function(){
+    if($(this).is(':checked')){
+        $('#attendCy').show(400)
+    }
+    else{
+        $('#attendCy').hide(400);
+        $('#txtAttendCy').val(0);
+    }
+});
+
+$('#chkTp').change(function(){
+    if($(this).is(':checked')){
+        $('#attendTp').show(400)
+    }
+    else{
+        $('#attendTp').hide(400);
+        $('#txtAttendTp').val(0);
+    }
+});
+
+$(document).ready(function () {
+    $('#wed_img').delay(200).animate({'opacity':'1'},700);
+    $('.formcontain').delay(600).animate({'opacity':'1'},600);
+});
